78613d892627f8689148bb5ca6e68c80f947d733
blackplusy <osimeno@163.com>
2020-08-17 10:39:28 +0800

    Add files via upload

---
+++ 例子-0817-02.python的输入.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+#输入是把input函数的值传入到变量中，操作变量
+#input()括号中填写提示信息
+#name=input()
+#print('your name is %s'% name)
+
+#name=input('请输入您的法号: ')
+#print('您的法号是%s'% name)
+
+
+#num1=int(input('请输入您本月薪资： '))
+#num2=int(input('请输入您上月薪资: '))
+#print(num1+num2)
+
+num3=input('your num')
+print(type(num3))
+num3=int(input('your num'))
+print(type(num3))
