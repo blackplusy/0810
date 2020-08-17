b5bccf3c7ac90da5b8ebdbba50fa12d813b9313b
blackplusy <osimeno@163.com>
2020-08-17 16:27:31 +0800

    Add files via upload

---
+++ 例子-0817-08.for循环.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+#字符串
+for i in 'abcd':
+    print(i)
+
+#列表
+l=[1,2,'heygor','ladeng']
+for a in l:
+    print(a)
+
+#函数
+#内置函数range()
+#range(10)   0-9
+#range(1,10) 1-9
+'''
+for i in range(10):
+    print(i)
+
+for i in range(1,10):
+    print(i)
+'''
+for i in range(-5,6):
+    if i<0:
+        print(-i)
+    else:
+        print(i)
+
+print('*'*10)
