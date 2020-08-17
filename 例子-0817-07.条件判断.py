f17f1928c6d21617717f1de9400cb3d92767f0ee
blackplusy <osimeno@163.com>
2020-08-17 14:39:03 +0800

    Add files via upload

---
+++ 例子-0817-07.条件判断.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+#单分支判断
+'''
+name=input('请输入您大表哥的名字: ')
+if name=='蔡徐坤':
+    print('唱跳rap篮球，及你太美')
+'''
+
+#双分支判断
+'''
+name=input('为什么叫我贝塔？')
+if name=='巧碧螺':
+    print('itsme baby!')
+else:
+    print('舒克的飞机刷没了！！')
+'''
+
+#多分支判断
+name=input('您喜欢的主播是: ')
+if name=='醉叮当':
+    print('来，刷火箭！！！！')
+elif name=='55kai':
+    print('难受啊，马飞！！！')
+elif name=='大司马':
+    print('正方形打野')
+else:
+    print('爱你们哟，没点关注点播关注！')
+    
